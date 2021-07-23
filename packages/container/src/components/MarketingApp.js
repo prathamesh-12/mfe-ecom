e656e3c2afab365ecf0231181cc50ff8bcf2f2b7
Prathamesh V. Nimbkar <pratha662537@majesco.com>
2021-07-23 10:42:05 +0530

    #5 - Marketing App changes

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -3,6 +3,7 @@ import { mount } from "marketing/MarketingApp";
 
 const MarketingApp = () => {
   const ref = useRef(null);
+  console.log("Markerting Compoenent is loaded!");
 
   useEffect(() => {
     mount(ref.current);
